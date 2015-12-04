3352edbe0609ed5103079f3849c337b8755bd458
Connor Workman <connorworkman@users.noreply.github.com>
2015-12-04 10:23:58 -0800

    code cleanup, commenting

--- util.cpp
+++ util.cpp
@@ -2,8 +2,8 @@
 #include <algorithm>
 #include <sstream>
 #include <vector>
-
-std::string ToString(const XEvent& e) {
+using namespace std;
+string ToString(const XEvent& e) {
   static const char* const X_EVENT_TYPE_NAMES[] = {
       "",
       "",
@@ -42,148 +42,88 @@ std::string ToString(const XEvent& e) {
       "MappingNotify",
   };
 
-  // 1. Compile properties we care about.
-  std::vector<std::pair<std::string, std::string>> properties;
+  vector<pair<string, string>> properties;
   switch (e.type) {
     case CreateNotify:
-      properties.emplace_back(
-          "window", ToString(e.xcreatewindow.window));
-      properties.emplace_back(
-          "parent", ToString(e.xcreatewindow.parent));
-      properties.emplace_back(
-          "size",
-          Size<int>(e.xcreatewindow.width, e.xcreatewindow.height).ToString());
-      properties.emplace_back(
-          "position",
-          Position<int>(e.xcreatewindow.x, e.xcreatewindow.y).ToString());
-      properties.emplace_back(
-          "border_width",
-          ToString(e.xcreatewindow.border_width));
-      properties.emplace_back(
-          "override_redirect",
-          ToString(static_cast<bool>(e.xcreatewindow.override_redirect)));
+      properties.emplace_back("window", ToString(e.xcreatewindow.window));
+      properties.emplace_back("parent", ToString(e.xcreatewindow.parent));
+      properties.emplace_back("size", Size<int>(e.xcreatewindow.width, e.xcreatewindow.height).ToString());
+      properties.emplace_back("position", Position<int>(e.xcreatewindow.x, e.xcreatewindow.y).ToString());
+      properties.emplace_back("border_width", ToString(e.xcreatewindow.border_width));
+      properties.emplace_back("override_redirect",ToString(static_cast<bool>(e.xcreatewindow.override_redirect)));
       break;
     case DestroyNotify:
-      properties.emplace_back(
-          "window", ToString(e.xdestroywindow.window));
-      break;
-    case MapNotify:
-      properties.emplace_back(
-          "window", ToString(e.xmap.window));
-      properties.emplace_back(
-          "event", ToString(e.xmap.event));
-      properties.emplace_back(
-          "override_redirect",
-          ToString(static_cast<bool>(e.xmap.override_redirect)));
-      break;
-    case UnmapNotify:
-      properties.emplace_back(
-          "window", ToString(e.xunmap.window));
-      properties.emplace_back(
-          "event", ToString(e.xunmap.event));
-      properties.emplace_back(
-          "from_configure",
-          ToString(static_cast<bool>(e.xunmap.from_configure)));
-      break;
-    case ConfigureNotify:
-      properties.emplace_back(
-          "window", ToString(e.xconfigure.window));
-      properties.emplace_back(
-          "size",
-          Size<int>(e.xconfigure.width, e.xconfigure.height).ToString());
-      properties.emplace_back(
-          "position",
-          Position<int>(e.xconfigure.x, e.xconfigure.y).ToString());
-      properties.emplace_back(
-          "border_width",
-          ToString(e.xconfigure.border_width));
-      properties.emplace_back(
-          "override_redirect",
-          ToString(static_cast<bool>(e.xconfigure.override_redirect)));
-      break;
-    case ReparentNotify:
-      properties.emplace_back(
-          "window", ToString(e.xreparent.window));
-      properties.emplace_back(
-          "parent", ToString(e.xreparent.parent));
-      properties.emplace_back(
-          "position",
-          Position<int>(e.xreparent.x, e.xreparent.y).ToString());
-      properties.emplace_back(
-          "override_redirect",
-          ToString(static_cast<bool>(e.xreparent.override_redirect)));
-      break;
-    case MapRequest:
-      properties.emplace_back(
-          "window", ToString(e.xmaprequest.window));
-      break;
-    case ConfigureRequest:
-      properties.emplace_back(
-          "window", ToString(e.xconfigurerequest.window));
-      properties.emplace_back(
-          "parent", ToString(e.xconfigurerequest.parent));
-      properties.emplace_back(
-          "value_mask",
-          XConfigureWindowValueMaskToString(e.xconfigurerequest.value_mask));
-      properties.emplace_back(
-          "position",
-          Position<int>(e.xconfigurerequest.x,
-                        e.xconfigurerequest.y).ToString());
-      properties.emplace_back(
-          "size",
-          Size<int>(e.xconfigurerequest.width,
-                    e.xconfigurerequest.height).ToString());
-      properties.emplace_back(
-          "border_width",
-          ToString(e.xconfigurerequest.border_width));
-      break;
-    case ButtonPress:
-    case ButtonRelease:
-      properties.emplace_back(
-          "window", ToString(e.xbutton.window));
-      properties.emplace_back(
-          "button", ToString(e.xbutton.button));
-      properties.emplace_back(
-          "position_root",
-          Position<int>(e.xbutton.x_root, e.xbutton.y_root).ToString());
+      properties.emplace_back("window", ToString(e.xdestroywindow.window));
       break;
     case MotionNotify:
-      properties.emplace_back(
-          "window", ToString(e.xmotion.window));
-      properties.emplace_back(
-          "position_root",
-          Position<int>(e.xmotion.x_root, e.xmotion.y_root).ToString());
-      properties.emplace_back(
-          "state", ToString(e.xmotion.state));
-      properties.emplace_back(
-          "time", ToString(e.xmotion.time));
+      properties.emplace_back("window", ToString(e.xmotion.window));
+      properties.emplace_back("position_root",Position<int>(e.xmotion.x_root, e.xmotion.y_root).ToString());
+      properties.emplace_back("state", ToString(e.xmotion.state));
+      properties.emplace_back("time", ToString(e.xmotion.time));
+      break;
+    case MapNotify:
+      properties.emplace_back("window", ToString(e.xmap.window));
+      properties.emplace_back("event", ToString(e.xmap.event));
+      properties.emplace_back("override_redirect",ToString(static_cast<bool>(e.xmap.override_redirect)));
+      break;
+    case UnmapNotify:
+      properties.emplace_back("window", ToString(e.xunmap.window));
+      properties.emplace_back("event", ToString(e.xunmap.event));
+      properties.emplace_back("from_configure", ToString(static_cast<bool>(e.xunmap.from_configure)));
+      break;
+    case ConfigureNotify:
+      properties.emplace_back("window", ToString(e.xconfigure.window));
+      properties.emplace_back("size", Size<int>(e.xconfigure.width, e.xconfigure.height).ToString());
+      properties.emplace_back("position", Position<int>(e.xconfigure.x, e.xconfigure.y).ToString());
+      properties.emplace_back("border_width", ToString(e.xconfigure.border_width));
+      properties.emplace_back("override_redirect", ToString(static_cast<bool>(e.xconfigure.override_redirect)));
+      break;
+    case ReparentNotify:
+      properties.emplace_back("window", ToString(e.xreparent.window));
+      properties.emplace_back("parent", ToString(e.xreparent.parent));
+      properties.emplace_back("position", Position<int>(e.xreparent.x, e.xreparent.y).ToString());
+      properties.emplace_back("override_redirect", ToString(static_cast<bool>(e.xreparent.override_redirect)));
+      break;
+    case MapRequest:
+      properties.emplace_back("window", ToString(e.xmaprequest.window));
+      break;
+    case ConfigureRequest:
+      properties.emplace_back("window", ToString(e.xconfigurerequest.window));
+      properties.emplace_back("parent", ToString(e.xconfigurerequest.parent));
+      properties.emplace_back("value_mask",XConfigureWindowValueMaskToString(e.xconfigurerequest.value_mask));
+      properties.emplace_back("position",Position<int>(e.xconfigurerequest.x, e.xconfigurerequest.y).ToString());
+      properties.emplace_back("size", Size<int>(e.xconfigurerequest.width, e.xconfigurerequest.height).ToString());
+      properties.emplace_back("border_width", ToString(e.xconfigurerequest.border_width));
+      break;
+    case ButtonPress://trickle down to ButtonRelease
+    case ButtonRelease:
+      properties.emplace_back("window", ToString(e.xbutton.window));
+      properties.emplace_back("button", ToString(e.xbutton.button));
+      properties.emplace_back("position_root", Position<int>(e.xbutton.x_root, e.xbutton.y_root).ToString());
       break;
     case KeyPress:
     case KeyRelease:
-      properties.emplace_back(
-          "window", ToString(e.xkey.window));
-      properties.emplace_back(
-          "state", ToString(e.xkey.state));
-      properties.emplace_back(
-          "keycode", ToString(e.xkey.keycode));
+      properties.emplace_back("window", ToString(e.xkey.window));
+      properties.emplace_back("state", ToString(e.xkey.state));
+      properties.emplace_back("keycode", ToString(e.xkey.keycode));
       break;
     default:
-      // No properties are printed for unused events.
+      //FIXME there are many unused event types to handle
       break;
   }
 
-  // 2. Build final string.
-  const std::string properties_string = Join(
-      properties, ", ", [] (const std::pair<std::string, std::string> &pair) {
+  /* Print debugging info for each event. */
+  const string properties_string = Join(
+      properties, ", ", [] (const pair<string, string> &pair) {
         return pair.first + ": " + pair.second;
       });
-  std::ostringstream out;
-  out << X_EVENT_TYPE_NAMES[e.type] << " { " << properties_string << " }";
+  ostringstream out;
+  out << X_EVENT_TYPE_NAMES[e.type] << " ## " << properties_string << " ##";
   return out.str();
 }
 
-std::string XConfigureWindowValueMaskToString(unsigned long value_mask) {
-  std::vector<std::string> masks;
+string XConfigureWindowValueMaskToString(unsigned long value_mask) {
+  vector<string> masks;
   if (value_mask & CWX) {
     masks.emplace_back("X");
   }
@@ -208,7 +148,7 @@ std::string XConfigureWindowValueMaskToString(unsigned long value_mask) {
   return Join(masks, "|");
 }
 
-std::string XRequestCodeToString(unsigned char request_code) {
+string XRequestCodeToString(unsigned char request_code) {
   static const char* const X_REQUEST_CODE_NAMES[] = {
       "",
       "CreateWindow",
